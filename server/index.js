c9c06ac42939960f100dffe64743c8bcc475b9ef
Efrat Harel <efratha94@gmail.com>
2021-07-27 12:40:15 +0300

    trying again

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const path = require("path")
 const mongoose = require("mongoose")
 const api = require("../server/api/api")
 
+
 const dbConnectionOpts = {
   socketTimeoutMS: 0,
   connectTimeoutMS: 0,
